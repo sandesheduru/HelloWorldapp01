2d79c84b85cef76588ec4e90633f8d827a7ad31c
Rahul Reghunath <91956111+rahulreghu-mns@users.noreply.github.com>
2021-10-17 00:21:06 +0530

    Update ScoreTest.java

--- src/test/java/ScoreTest.java
+++ src/test/java/ScoreTest.java
@@ -6,9 +6,28 @@ import org.junit.Test;
 public class ScoreTest {
 
 @Test
-	public void testToString() {
+	public void testToString1() {
 	
 		Assert.assertTrue(1>0);
 	}
+	public void testToString2() {
+	
+		Assert.assertTrue(1>0);
+	}
+
+	public void testToString3() {
+	
+		Assert.assertTrue(1>0);
+	}
+	public void testToString4() {
+	
+		Assert.assertTrue(1>0);
+	}
+
+	public void testToString5() {
+	
+		Assert.assertTrue(1>2);
+	}
+
 
 }
